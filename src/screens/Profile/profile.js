47980da264acd7c702d94d5561ee0aa822645c13
swexc <swexc6006@gmail.com>
2021-03-02 12:47:24 +0300

    re

--- src/screens/Profile/profile.js
+++ src/screens/Profile/profile.js
@@ -11,7 +11,7 @@ export default class profile extends Component {
   render() {
     return (
       <View style={{alignItems:"center",justifyContent:"center",flex:1,backgroundColor: "white",}}>
-        <Text> User  </Text>
+        <Text> User Profile </Text>
       </View>
     );
   }
